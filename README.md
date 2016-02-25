b6d6142f424aa4d997645112a8f0fce4362801b3
Scriptgate <wscheppers@hotmail.com>
2016-02-25 23:10:13 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Scriptgate Repository
---
+++ mvn-install.sh
@@ -0,0 +1,23 @@
+mvn install:install-file \
+ -DgroupId=net.scriptgate.engine \
+ -DartifactId=engine-api \
+ -Dversion=0.0.1-SNAPSHOT \
+ -Dpackaging=jar \
+ -Dfile=engine-api-0.0.1-SNAPSHOT.jar \
+ -DlocalRepositoryPath=.
+
+mvn install:install-file \
+ -DgroupId=net.scriptgate.engine \
+ -DartifactId=engine-core \
+ -Dversion=0.0.1-SNAPSHOT \
+ -Dpackaging=jar \
+ -Dfile=engine-core-0.0.1-SNAPSHOT.jar \
+ -DlocalRepositoryPath=.
+
+mvn install:install-file \
+ -DgroupId=net.scriptgate.engine \
+ -DartifactId=engine-lwjgl \
+ -Dversion=0.0.1-SNAPSHOT \
+ -Dpackaging=jar \
+ -Dfile=engine-lwjgl-0.0.1-SNAPSHOT.jar \
+ -DlocalRepositoryPath=.
